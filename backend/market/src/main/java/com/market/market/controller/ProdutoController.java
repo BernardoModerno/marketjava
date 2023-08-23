bdcd0b082a65a6c16f48cb64f83174e4825cf230
BernardoModerno <bernardo.moderno@gmail.com>
2023-08-23 20:38:45 -0300

    controller Produto

---
+++ backend/market/src/main/java/com/market/market/controller/ProdutoController.java
@@ -0,0 +1,50 @@
+package com.market.market.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.market.market.entity.Produto;
+import com.market.market.service.ProdutoService;
+
+@RestController
+@RequestMapping("/api")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping("/produto")
+    public List<Produto> findAll() {
+        return produtoService.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    public Produto findById(@PathVariable("id") String id) {
+        return produtoService.findById(id);
+    }
+
+    @PostMapping("/produto")
+    public Produto create(@RequestBody Produto produto) {
+        return produtoService.create(produto);
+    }
+
+    @PutMapping("/produto")
+    public Produto edit(@RequestBody Produto produto) {
+        return produtoService.edit(produto);
+    }
+
+    @DeleteMapping("/produto/{id}")
+    public void deleteById(@PathVariable("id") String id) {
+        produtoService.deleteById(id);
+    }
+    
+}
--- backend/market/src/main/java/com/market/market/service/ProdutoService.java
+++ backend/market/src/main/java/com/market/market/service/ProdutoService.java
@@ -5,13 +5,19 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.market.market.entity.Produto;
+import com.market.market.exception.BadRequestException;
 import com.market.market.exception.ResourceNotFoundException;
+import com.market.market.repository.CategoriaRepository;
 import com.market.market.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
     
     @Autowired
     private ProdutoRepository produtoRepository;
@@ -26,11 +32,45 @@ public class ProdutoService {
     }
 
     public Produto create(Produto produto) {
+        if (!StringUtils.hasText(produto.getNome())) {
+            throw new BadRequestException("O nome do produto não pode ficar vazio.");
+        }
+
+        if (produto.getCategoria() == null) {
+            throw new BadRequestException("A categoria não pode ficar vazia.");
+        }
+
+        if (!StringUtils.hasText(produto.getCategoria().getId())) {
+            throw new BadRequestException("O Id da categoria não pode ficar vazio.");
+        }
+
+        categoriaRepository.findById(produto.getCategoria().getId())
+                .orElseThrow(() -> new BadRequestException(
+                        "Categoria ID " + produto.getCategoria().getId() + " não encontrado no banco de dados."));
         produto.setId(UUID.randomUUID().toString());
         return produtoRepository.save(produto);
     }
 
     public Produto edit(Produto produto) {
+        if (!StringUtils.hasText(produto.getId())) {
+            throw new BadRequestException("O ID do produto é obrigatório.");
+        }
+
+        if (!StringUtils.hasText(produto.getNome())) {
+            throw new BadRequestException("O nome do produto não pode ficar vazio.");
+        }
+
+        if (produto.getCategoria() == null) {
+            throw new BadRequestException("A categoria não pode ficar vazia.");
+        }
+
+        if (!StringUtils.hasText(produto.getCategoria().getId())) {
+            throw new BadRequestException("A categoria de ID não pode ficar vazia.");
+        }
+
+        categoriaRepository.findById(produto.getCategoria().getId())
+                .orElseThrow(() -> new BadRequestException(
+                        "Categoria ID " + produto.getCategoria().getId() + " não encontrado no banco de dados."));
         return produtoRepository.save(produto);
     }
 
