0460ba5903478759d25604b7c1b0f12f00b6c27b
BernardoModerno <bernardo.moderno@gmail.com>
2023-08-26 18:52:28 -0300

    foto produto Java

---
+++ backend/market/src/main/java/com/market/market/FileStorageProperties.java
@@ -0,0 +1,13 @@
+package com.market.market;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@ConfigurationProperties(prefix = "file")
+@Data
+public class FileStorageProperties {
+
+    private String uploadDir;
+
+}
--- backend/market/src/main/java/com/market/market/MarketApplication.java
+++ backend/market/src/main/java/com/market/market/MarketApplication.java
@@ -2,8 +2,12 @@ package com.market.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class MarketApplication {
 
 	public static void main(String[] args) {
