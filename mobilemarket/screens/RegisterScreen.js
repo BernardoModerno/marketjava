363d3759e35006d6072496603f2cdfbb64af022a
BernardoModerno <bernardo.moderno@gmail.com>
2023-09-12 10:04:21 -0300

    Login Screen

---
+++ mobilemarket/screens/RegisterScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <Text>RegisterScreen</Text>
+    </View>
+  )
+}
+
+export default RegisterScreen
+
+const styles = StyleSheet.create({})
